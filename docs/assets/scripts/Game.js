18f2a55941184e373f46f3be9d3a8e7590d4b77c
Assebe <Lambadina1>
2023-01-30 11:48:18 +0000

    Multiple changes in different classes

--- docs/assets/scripts/Game.js
+++ docs/assets/scripts/Game.js
@@ -7,6 +7,8 @@ class Game{
         this.player = player
         this.isIntervalID = null;
         this.frames = 0;
+        this.enemies = []
+        this.backgroundImage = new Image();
 
  }
  start(){
@@ -14,6 +16,58 @@ class Game{
     }
 
  update = () => {
-       
+    this.frames++;
+    this.clear();
+    this.player.newPos();
+    this.player.draw();
+    this.updateEnemies();
+    this.checkColision()
+    this.checkGameOver();
     }
+
+    stop() {
+        clearInterval(this.intervalId);
+      }
+
+      clear(){
+        this.ctx.clearRect(0, 0, this.width, this.height)
+    }
+    updateEnemies() {
+        for (let i = 0; i < this.enemies.length; i++) {
+          this.enemies[i].x -= 1;
+          this.enemies[i].draw();
+        }
+    
+        if (this.frames % 200 === 0) {
+          let randomSize = Math.floor(Math.random() * (100 - 50) + 150);
+    
+          let randomY = Math.floor(Math.random() * (200 - 100) + 100);
+    
+          this.enemies.push(new Enemy(1200, randomY, randomSize, 50, "red", this.ctx));
+        }
+      }
+    checkColision() {
+        const crashed = this.enemies.some((enemy) => {
+          return this.player.crashWith(enemy);
+        });
+    
+        if (crashed) {
+          ctx.fillStyle = "black";
+          ctx.fillRect(50, 200, 400, 250);
+          ctx.font = "32px Helvetica";
+          ctx.fillStyle = "red";
+          ctx.fillText(`Colision`, 150, 300);
+          ctx.fillStyle = "white";
+        }
+      }
+    
+      checkGameOver(){
+        const timeRunout = this.frames> 5000
+      }
+      if (timeRunout) {
+        ctx.font = "32px Verdana";
+        ctx.fillStyle = "red";
+        ctx.fillText(`Game Over :(  Time ran out`, 150, 300);
+        this.stop()
+      }
 }
--- docs/assets/scripts/Main.js
+++ docs/assets/scripts/Main.js
@@ -5,7 +5,19 @@ const ctx  = canvas.getContext("2d");
 
 const startButton = document.getElementById("startButton")
 
+const player = new Component(10, canvas.height/2 - 45, 75, 75, "red", ctx)
+
 startButton.onclick = function(){
     const game = new Game(ctx, canvas.width, canvas.height, player)
     game.start()
 }
+
+document.addEventListener("keydown", (e) => {
+    switch(e.code){
+    
+        case "ArrowUp": player.speedY -= 1; break;
+    
+        case "ArrowDown": player.speedY += 1; break;
+
+    }
+    })
