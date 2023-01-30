99089b31151a20ba85b6c4788640445fba5a8afa
miacsilva <miacsilvac@hotmail.com>
2023-01-30 17:37:23 +0000

    more changes

--- docs/assets/scripts/Game.js
+++ docs/assets/scripts/Game.js
@@ -8,9 +8,12 @@ class Game{
         this.isIntervalID = null;
         this.frames = 0;
         this.enemies = []
+        this.verticalEnemies = []
+        this.bonusItems = []
         this.backgroundImage = new Image();
+      }
 
- }
+
  start(){
     this.isIntervalID = setInterval(this.update, 1000/60)
     }
@@ -21,53 +24,99 @@ class Game{
     this.player.newPos();
     this.player.draw();
     this.updateEnemies();
-    this.checkColision()
+    this.updateVerticalEnemies();
+    this.updateBonusItems();
+    this.checkColision();
+    this.checkGameWon();
     this.checkGameOver();
     }
 
-    stop() {
-        clearInterval(this.intervalId);
+ stop() {
+        clearInterval(this.isIntervalID)
       }
 
-      clear(){
+ clear(){
         this.ctx.clearRect(0, 0, this.width, this.height)
     }
-    updateEnemies() {
+
+  updateEnemies() {
         for (let i = 0; i < this.enemies.length; i++) {
-          this.enemies[i].x -= 1;
+          this.enemies[i].x -= 3;
           this.enemies[i].draw();
         }
     
-        if (this.frames % 200 === 0) {
-          let randomSize = Math.floor(Math.random() * (100 - 50) + 150);
+        if (this.frames % 0 === 0) {
     
-          let randomY = Math.floor(Math.random() * (200 - 100) + 100);
+          let randomY = Math.floor(Math.random() * (600 - 50) + 50);
     
-          this.enemies.push(new Enemy(1200, randomY, randomSize, 50, "red", this.ctx));
+          this.enemies.push(new Enemy(1200, randomY, 40, 40, "green", this.ctx));
         }
       }
-    checkColision() {
-        const crashed = this.enemies.some((enemy) => {
+
+      updateVerticalEnemies() {
+        for (let i = 0; i < this.verticalEnemies.length; i++) {
+          this.verticalEnemies[i].x -= 3;
+          this.verticalEnemies[i].y -= 0.5;
+          this.verticalEnemies[i].draw();
+        }
+    
+        if (this.frames % 70 === 0) {
+    
+          let randomY = Math.floor(Math.random() * (600 - 50) + 50);
+          this.verticalEnemies.push(new Enemy(1200, randomY, 40, 40, "green", this.ctx));
+        }
+      }
+
+  updateBonusItems() {
+        for (let i = 0; i < this.bonusItems.length; i++) {
+          this.bonusItems[i].x -= 1;
+          this.bonusItems[i].draw();
+        }
+    
+        if (this.frames % 1000 === 0) {
+    
+          let randomY = Math.floor(Math.random() * (600 - 100) + 100);
+    
+          this.bonusItems.push(new BonusItem(1200, randomY, 30, 20, "yellow", this.ctx));
+        }
+      }
+
+  checkColision() {
+        const crashedEnemies = this.enemies.some((enemy) => {
           return this.player.crashWith(enemy);
         });
     
-        if (crashed) {
-          ctx.fillStyle = "black";
-          ctx.fillRect(50, 200, 400, 250);
-          ctx.font = "32px Helvetica";
-          ctx.fillStyle = "red";
-          ctx.fillText(`Colision`, 150, 300);
-          ctx.fillStyle = "white";
+        if (crashedEnemies) {
+          this.stop()
         }
       }
     
-      checkGameOver(){
-        const timeRunout = this.frames> 5000
-      }
-      if (timeRunout) {
-        ctx.font = "32px Verdana";
-        ctx.fillStyle = "red";
-        ctx.fillText(`Game Over :(  Time ran out`, 150, 300);
-        this.stop()
-      }
+
+  checkGameWon(){
+    if(this.player.x >= canvas.width){
+     ctx.font = "32px Verdana";
+     ctx.fillStyle = "red";
+     ctx.fillText(`You made it!`, canvas.width/2 -100, canvas.height/2);
+     this.stop()
+    }
+  }
+
+  checkGameOver(){
+     if (this.frames >= 3600){
+     ctx.font = "32px Verdana";
+     ctx.fillStyle = "red";
+     ctx.fillText(`Game Over, train left :(`, canvas.width/2 -100, canvas.height/2);
+     this.stop()
+        }
+}
+
+checkifTouched(){
+  const touched = this.bonusItems.some((bonusItem) => {
+      return this.player.touchBonus(bonusItem);
+  })
+  if(touched){
+      setInterval(this.player.speedX += 0.1, 4000)
+      setInterval(this.player.speedY += 0.1, 4000)
+  }
+}
 }
--- docs/assets/scripts/Main.js
+++ docs/assets/scripts/Main.js
@@ -3,21 +3,37 @@
 const canvas = document.getElementById("canvas")
 const ctx  = canvas.getContext("2d");
 
+
+
 const startButton = document.getElementById("startButton")
 
-const player = new Component(10, canvas.height/2 - 45, 75, 75, "red", ctx)
+const player = new Component(10, canvas.height/2 - 45, 40, 40, "red", ctx)
 
-startButton.onclick = function(){
-    const game = new Game(ctx, canvas.width, canvas.height, player)
-    game.start()
-}
-
+startButton.onclick = function () {
+    const game = new Game(ctx, canvas.width, canvas.height, player);
+    game.start();
+  };
+  
 document.addEventListener("keydown", (e) => {
     switch(e.code){
     
-        case "ArrowUp": player.speedY -= 1; break;
+        case "ArrowUp":
+        if (player.y > 25) {
+            player.speedY -= 0.5;
+          } else {
+            player.speedY = 0;
+          }
+          break;
     
-        case "ArrowDown": player.speedY += 1; break;
-
-    }
+        case "ArrowDown": 
+        if (player.y + player.speedY < canvas.height -85){
+        player.speedY += 0.2;
+        } else {
+        player.speedY = 0;
+        }
+        break;
+        }
+    })
+document.addEventListener("keyup", (e) =>{
+        player.speedY = 0
     })
